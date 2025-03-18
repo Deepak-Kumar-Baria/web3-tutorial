b402c225d613c1007f413a9a923a89f9ae665ffa
Deepak-Kumar-Baria <dbaria@my.bridgeport.edu>
2025-03-17 22:35:08 -0400

    final version

--- src/Interact.py
+++ src/Interact.py
@@ -2,38 +2,37 @@ from web3 import Web3
 from Deploy import deploy_contract
 import os
 
-contract_file = "./src/SimpleStorage.sol"
+contract_file = "./src/newContract.sol"
+contract_name= 'newContract'
 account = os.getenv("ANVIL_ACCOUNT")
 private_key = os.getenv("ANVIL_PRIVATE_KEY")
 provider = os.getenv("LOCAL_PROVIDER")
-print(provider)
 chain_id = 31337
 
 connection = Web3(Web3.HTTPProvider(provider))
 
-contract_address, abi = deploy_contract(contract_file, "SimpleStorage", account, private_key, provider, chain_id)  
+contract_address, abi = deploy_contract(contract_file,"newContract",account,private_key,provider,chain_id)
 print(f"Contract deployed at {contract_address}")
 
 simple_storage = connection.eth.contract(address=contract_address, abi=abi)
 nonce = connection.eth.get_transaction_count(account)
 
-print("Creating Transaction")
-transaction = simple_storage.functions.set(150032145).build_transaction(
+print("Creating Transactions")
+transaction = simple_storage.functions.updateID(5341).build_transaction(
     {
-            "chainId": chain_id,
-            "gasPrice": connection.eth.gas_price,
-            "from": account,
-            "nonce": nonce
-        }
-)
+        "chainId": chain_id,
+        "gasPrice": connection.eth.gas_price,
+        "from": account,
+        "nonce": nonce
 
+    }
+)
 signed_txn = connection.eth.account.sign_transaction(transaction, private_key=private_key)
 
-print("Updating stored value")
+print("Updating stored Value")
 tx_hash = connection.eth.send_raw_transaction(signed_txn.raw_transaction)
 
 tx_receipt = connection.eth.wait_for_transaction_receipt(tx_hash)
-print("Updated")
-
-updated_value = simple_storage.functions.get().call()
+print("updated")
+updated_value = simple_storage.functions.viewMyId().call()
 print(f"Updated value is {updated_value}")
--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,14 +1,17 @@
+
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
 contract SimpleStorage {
     // State Variable
     uint256 public data;
-    
-    function set (uint256 _data) public{
+
+
+    function set(uint256 _data) public {
         data = _data;
-        }
-    function get() public view returns (uint256){
+    }
+
+    function get() public view returns (uint256) {
         return data;
     }
 }
